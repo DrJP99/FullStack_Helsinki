564c568ceb76fce7e6410dc2be9148124dcc6bab
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-07-05 15:02:51 -0600

    1.3

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -13,47 +13,56 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
-	const part1 = props.parts[0];
-	const part2 = props.parts[1];
-	const part3 = props.parts[2];
+	// const part1 = props.parts[0];
+	// const part2 = props.parts[1];
+	// const part3 = props.parts[2];
 
-	const exercises1 = props.excercises[0];
-	const exercises2 = props.excercises[1];
-	const exercises3 = props.excercises[2];
+	const [part1, part2, part3] = props.parts;
+	// const [excercises1, excercises2, excercises3] = props.excercises;
+
+	// const exercises1 = props.excercises[0];
+	// const exercises2 = props.excercises[1];
+	// const exercises3 = props.excercises[2];
 	return (
 		<>
-			<Part part={part1} excercises={exercises1} />
-			<Part part={part2} excercises={exercises2} />
-			<Part part={part3} excercises={exercises3} />
+			<Part part={part1.name} excercises={part1.exercises} />
+			<Part part={part2.name} excercises={part2.exercises} />
+			<Part part={part3.name} excercises={part3.exercises} />
 		</>
 	);
 };
 
 const Total = (props) => {
 	let total = 0;
-	props.excercises.forEach((element) => {
-		total += element;
-	});
+	for (let i in props.parts) {
+		total += props.parts[i].exercises;
+	}
 	return <p>Number of exercises {total}</p>;
 };
 
 const App = () => {
 	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
-	const exercises3 = 14;
+	const part1 = {
+		name: "Fundamentals of React",
+		exercises: 10,
+	};
+	const part2 = {
+		name: "Using props to pass data",
+		exercises: 7,
+	};
+	const part3 = {
+		name: "State of a component",
+		exercises: 14,
+	};
 
 	const parts = [part1, part2, part3];
-	const excercises = [exercises1, exercises2, exercises3];
+	// const excercises = [exercises1, exercises2, exercises3];
 
 	return (
 		<div>
 			<Header course={course} />
-			<Content parts={parts} excercises={excercises} />
-			<Total excercises={excercises} />
+			<Content parts={parts} />
+			<Total parts={parts} />
 		</div>
 	);
 };
