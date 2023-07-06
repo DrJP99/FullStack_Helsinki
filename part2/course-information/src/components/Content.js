72d9340ab9097c155cf922dc691e4916044ba45e
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-07-06 12:01:54 -0600

    2.3

--- part2/course-information/src/components/Content.js
+++ part2/course-information/src/components/Content.js
@@ -1,11 +1,19 @@
 import Part from "./Part";
 
 const Content = ({ parts }) => {
+	const total = parts.reduce((s, p) => {
+		// console.log(s, p.exercises);
+		return s + p.exercises;
+	}, 0);
+	// console.log(total);
 	return (
 		<>
 			{parts.map((part) => (
 				<Part key={part.id} part={part} />
 			))}
+			<p>
+				<b>Number of exercises {total}</b>
+			</p>
 		</>
 	);
 };
