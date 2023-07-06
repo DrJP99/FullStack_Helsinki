aecdd7da8641e125ee71653503fec27b54466522
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-07-06 14:09:06 -0600

    2.11

--- tests/src/App.js
+++ tests/src/App.js
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Note from "./components/Note";
 
-const App = (props) => {
+const App = () => {
 	// const { notes } = props;
-	const [notes, setNotes] = useState(props.notes);
+	const [notes, setNotes] = useState([]);
 	const [newNote, setNewNote] = useState("a new note...");
 	const [showAll, setShowAll] = useState(true);
+
+	const hook = () => {
+		console.log("effect");
+		axios.get("http://localhost:3001/notes").then((res) => {
+			console.log("promise fulfilled");
+			setNotes(res.data);
+		});
+	};
+
+	useEffect(hook, []);
+	console.log("render", notes.length, "notes");
 
 	const addNote = (event) => {
 		event.preventDefault();
