59dda5387cb6a6329357046a1196381f44c1364d
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-07-09 17:43:38 -0600

    4.5

--- part4/blog-list/tests/dummy.test.js
+++ part4/blog-list/tests/dummy.test.js
@@ -6,3 +6,91 @@ test("dummy returns one", () => {
 	const result = listHelper.dummy(blogs);
 	expect(result).toBe(1);
 });
+
+const listWithOneBlog = [
+	{
+		_id: "5a422aa71b54a676234d17f8",
+		title: "Go To Statement Considered Harmful",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+		likes: 5,
+		__v: 0,
+	},
+];
+
+const listWithManyBlogs = [
+	{
+		_id: "5a422aa71b54a676234d17f8",
+		title: "Go To Statement Considered Harmful",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+		likes: 5,
+		__v: 0,
+	},
+	{
+		_id: "5a422aa71b54a676232f17f9",
+		title: "On Fairy Stories",
+		author: "J.R.R. Tolkien",
+		url: "https://www.excellence-in-literature.com/on-fairy-stories-by-tolkien/",
+		likes: 10,
+		__v: 0,
+	},
+	{
+		_id: "5a422aa71b54a676232f17d0",
+		title: "What Is To Be Done?",
+		author: "Vladimir I. Lenin",
+		url: "https://www.marxists.org/archive/lenin/works/1901/witbd/",
+		likes: 15,
+		__v: 0,
+	},
+];
+
+const listWithZeroBlogs = [];
+
+describe("total likes", () => {
+	test("when list has one big blog, equals the likes of that", () => {
+		const result = listHelper.totalLikes(listWithOneBlog);
+		expect(result).toBe(5);
+	});
+
+	test("when list has many big blogs, equals the sum of likes", () => {
+		const result = listHelper.totalLikes(listWithManyBlogs);
+		expect(result).toBe(30);
+	});
+
+	test("when list has zero blogs, equals zero", () => {
+		const result = listHelper.totalLikes(listWithZeroBlogs);
+		expect(result).toBe(0);
+	});
+});
+
+describe("favorite blog", () => {
+	test("when list has one big blog, equals to itself", () => {
+		const result = listHelper.favoriteBlog(listWithOneBlog);
+		expect(result).toEqual({
+			_id: "5a422aa71b54a676234d17f8",
+			title: "Go To Statement Considered Harmful",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+			likes: 5,
+			__v: 0,
+		});
+	});
+
+	test("when list has many big blogs, equals to the one with most likes", () => {
+		const result = listHelper.favoriteBlog(listWithManyBlogs);
+		expect(result).toEqual({
+			_id: "5a422aa71b54a676232f17d0",
+			title: "What Is To Be Done?",
+			author: "Vladimir I. Lenin",
+			url: "https://www.marxists.org/archive/lenin/works/1901/witbd/",
+			likes: 15,
+			__v: 0,
+		});
+	});
+
+	test("when list has zero blogs, equals to null", () => {
+		const result = listHelper.favoriteBlog(listWithZeroBlogs);
+		expect(result).toEqual(null);
+	});
+});
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,30 @@ const dummy = (blogs) => {
 	return 1;
 };
 
+const totalLikes = (blogs) => {
+	let sum = 0;
+	blogs.forEach((blog) => {
+		sum += blog.likes;
+	});
+	return sum;
+};
+
+const favoriteBlog = (blogs) => {
+	let favorite = null;
+	let favorite_likes = 0;
+
+	blogs.forEach((blog) => {
+		if (blog.likes > favorite_likes) {
+			favorite = blog;
+			favorite_likes = blog.likes;
+		}
+	});
+
+	return favorite;
+};
+
 module.exports = {
 	dummy,
+	totalLikes,
+	favoriteBlog,
 };
