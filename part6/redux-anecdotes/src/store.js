2756196e96590e098f5c0b08d42490a316a863bf
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-07-12 15:57:21 -0600

    6.12

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import anecdoreReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 const store = configureStore({
 	reducer: {
 		anecdotes: anecdoreReducer,
 		filter: filterReducer,
+		notification: notificationReducer,
 	},
 })
 
