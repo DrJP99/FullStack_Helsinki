ced433e0226c7596d5c6bcf46395c051adbe52f4
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-07-12 19:16:59 -0600

    6.15

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,15 +1,17 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
+import anecdotesService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
 	const dispatch = useDispatch()
 
-	const newAnecdote = (e) => {
+	const newAnecdote = async (e) => {
 		e.preventDefault()
 		const content = e.target.anecdote.value
 		e.target.anecdote.value = ''
-		dispatch(createAnecdote(content))
+		const newAnecdote = await anecdotesService.createAnecdote(content)
+		dispatch(createAnecdote(newAnecdote))
 		dispatch(setNotification(`you created the anecdote '${content}'`))
 	}
 
--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -5,4 +5,16 @@ const getAll = async () => {
 	return (await axios.get(baseUrl)).data
 }
 
-export default { getAll }
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const createAnecdote = async (content) => {
+	const newAnecdote = {
+		content,
+		id: getId(),
+		votes: 0,
+	}
+	const res = await axios.post(baseUrl, newAnecdote)
+	return res.data
+}
+
+export default { getAll, createAnecdote }
