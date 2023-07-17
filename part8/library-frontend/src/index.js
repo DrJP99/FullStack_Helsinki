908176b957858b16043f886a4d9fa14bb53e175a
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-07-17 15:08:13 -0600

    8.9

--- part8/library-frontend/src/index.js
+++ part8/library-frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-import { ALL_AUTHORS } from './queries'
+import { ALL_AUTHORS, ALL_BOOKS } from './queries'
 
 const client = new ApolloClient({
 	uri: 'http://localhost:4000',
@@ -10,6 +10,7 @@ const client = new ApolloClient({
 })
 
 client.query({ query: ALL_AUTHORS }).then((res) => console.log(res.data))
+client.query({ query: ALL_BOOKS }).then((res) => console.log(res.data))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<ApolloProvider client={client}>
