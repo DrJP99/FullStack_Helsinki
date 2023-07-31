aa71c66c79e72056a29c49d04c0e4d401c7e3efa
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-07-31 13:25:52 -0600

    9.4

--- part9/BMI/index.ts
+++ part9/BMI/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 const app = express();
 
-app.get('/ping', (_req, res) => {
-	res.send('pong');
-});
-
 const PORT = 3003;
+
+app.get('/hello', (_req, res) => {
+	res.send('Hello Full Stack!');
+});
 
 app.listen(PORT, () => {
 	console.log(`Server running at ${PORT}`);
